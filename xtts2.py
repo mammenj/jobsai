c4fa06651633e93fe246ef868e457f899babfdcf
jsm <jsmammen@gmail.com>
2025-09-23 14:44:53 +0530

    xtts2

--- xtts2.py
+++ xtts2.py
@@ -1,31 +1,36 @@
 import torch
 from TTS.api import TTS
+import time
 
-# text2 = "    We talked of the side show in the circus.    Use a pencil to write the first draft.    He ran half way to the hardware store.    The clock struck to mark the third period.    A small creek cutacross the field.    Cars and busses stalled in snow drifts.    The set of china hit the floor with a crash.    This is a grand season for hikes on the road.    The dune rose from the edge of the water.   Those words were the cue for the actor to leave."
-
-
-text = ""
-with open("script.txt", "r") as file:
-    line = file.readline()
-    while line:
-        text += line
-        line = file.readline()
+start = time.perf_counter()
 
 
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Example voice cloning with YourTTS in English, French and Portuguese
-tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-# tts.tts_to_file(
-# text,
-# speaker_wav=["voicedir/myvoice.wav"],
-# speaker="jsmammen",
-# language="en",
-# )
 
-tts.tts_to_file(
-    text,
-    speaker="jsmammen",
-    language="en",
-)
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+
+count = 0
+with open("script.txt", "r") as file:
+    line = file.readline()
+    while line:
+        # text += line
+        print("Processing line: ", line)
+        count = count + 1
+        audio_name = "out/xtts_output_" + str(count) + ".wav"
+        line = file.readline()
+        if line.strip() != "" and line.__len__() > 3:
+            tts.tts_to_file(
+                text=line,
+                file_path=audio_name,
+                speaker="jsmammen",
+                language="en",
+                split_sentences=False,
+            )
+            print("Done with ", audio_name)
+
+
+end = time.perf_counter()
+print(f"Execution time: {end - start:0.4f} seconds")
